241a56b9d1dfd5e401b95a4d68c402b5eaae5b26
Angus McIntyre <gusdor@gmail.com>
2017-09-19 18:53:29 +0100

    Adds ability to parse single operator with regex

--- CalculatorLesson/Program.cs
+++ CalculatorLesson/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CalculatorLesson
@@ -12,9 +13,33 @@ namespace CalculatorLesson
         {
             while (true)
             {
-                Console.WriteLine("Please type a calculation and press ENTER.");
-                string input = Console.ReadLine();
-                Console.WriteLine($"You typed '{input}'.");
+                try
+                {
+                    Console.WriteLine("Please type a calculation and press ENTER.");
+                    string input = Console.ReadLine();
+                    int result = ParseWithRegex(input);
+                    Console.WriteLine($"Result: {result}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static int ParseWithRegex(string input)
+        {
+            var match = Regex.Match(input, @"(\d+)(\+)(\d+)");
+            if (match.Success)
+            {
+                int left = int.Parse(match.Groups[1].Value);
+                int right = int.Parse(match.Groups[3].Value);
+
+                return left + right;
+            }
+            else
+            {
+                throw new Exception("Input string must contain 2 operands and an operator.");
             }
         }
     }
