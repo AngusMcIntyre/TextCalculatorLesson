3cdae86560c1dc7d227f18c11e88a734cb541bb2
Angus McIntyre <gusdor@gmail.com>
2017-09-19 19:06:23 +0100

    Adds support for 4 basic arithmatic operations

--- CalculatorLesson/Program.cs
+++ CalculatorLesson/Program.cs
@@ -11,13 +11,21 @@ namespace CalculatorLesson
     {
         static void Main(string[] args)
         {
+            Operator[] operators = new []
+            {
+                new Operator { Symbol = '+', Operation = (left, right) => left + right },
+                new Operator { Symbol = '-', Operation = (left, right) => left - right },
+                new Operator { Symbol = '*', Operation = (left, right) => left * right },
+                new Operator { Symbol = '/', Operation = (left, right) => left / right },
+            };
+
             while (true)
             {
                 try
                 {
                     Console.WriteLine("Please type a calculation and press ENTER.");
                     string input = Console.ReadLine();
-                    int result = ParseWithRegex(input);
+                    int result = ParseWithRegex(input, operators);
                     Console.WriteLine($"Result: {result}");
                 }
                 catch (Exception ex)
@@ -27,20 +35,37 @@ namespace CalculatorLesson
             }
         }
 
-        private static int ParseWithRegex(string input)
+        private static int ParseWithRegex(string input, Operator[] availableOperators)
         {
-            var match = Regex.Match(input, @"(\d+)(\+)(\d+)");
+            string symbolRegex = string.Join("|", availableOperators.Select(p => "\\" + p.Symbol));
+            var match = Regex.Match(input, $@"(\d+)({symbolRegex})(\d+)");
             if (match.Success)
             {
                 int left = int.Parse(match.Groups[1].Value);
                 int right = int.Parse(match.Groups[3].Value);
 
-                return left + right;
+                Operator matchingOperator = availableOperators.FirstOrDefault(p => p.Symbol == match.Groups[2].Value[0]);
+
+                if (matchingOperator != null)
+                {
+                    return matchingOperator.Operation(left, right);
+                }
+                else
+                {
+                    throw new Exception("Operator not supported.");
+                }
             }
             else
             {
                 throw new Exception("Input string must contain 2 operands and an operator.");
             }
         }
+    }
+
+    class Operator
+    {
+        public char Symbol { get; set; }
+
+        public Func<int, int, int> Operation { get; set; }
     }
 }
