f1447b70cdbb21601fc5e27e4252db8198083978
Angus McIntyre <angus.mcintyre@exprogroup.com>
2017-09-19 16:27:26 +0100

    Adds error handling with exceptions

--- CalculatorLesson/Program.cs
+++ CalculatorLesson/Program.cs
@@ -12,25 +12,38 @@ namespace CalculatorLesson
         {
             while (true)
             {
-                Console.WriteLine("Please type a calculation and press ENTER.");
-                string input = Console.ReadLine();
-                Console.WriteLine($"You typed '{input}'.");
-
-                int result = 0;
-
-                // Parse the user input to get 2 operands and a an operator
-                string[] operands = input.Split('+');
-                if (operands.Length == 2)
+                try
                 {
-                    int left;
-                    int right;
-                    if (int.TryParse(operands[0], out left) && int.TryParse(operands[1], out right))
-                    {
-                        result = left + right;
-                    }
-                }
+                    Console.WriteLine("Please type a calculation and press ENTER.");
+                    string input = Console.ReadLine();
+                    Console.WriteLine($"You typed '{input}'.");
 
-                Console.WriteLine($"The result of your calculation is '{result}'.");
+                    int result = 0;
+
+                    // Parse the user input to get 2 operands and a an operator
+                    string[] operands = input.Split('+');
+
+                    if (operands.Length != 2)
+                    {
+                        throw new FormatException($"Input string must contain 2 operands. {operands.Length} were detected.");
+                    }
+
+                    if (operands.Length == 2)
+                    {
+                        int left;
+                        int right;
+                        if (int.TryParse(operands[0], out left) && int.TryParse(operands[1], out right))
+                        {
+                            result = left + right;
+                        }
+                    }
+
+                    Console.WriteLine($"The result of your calculation is '{result}'.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred - {ex.Message}");
+                }
             }
         }
     }
