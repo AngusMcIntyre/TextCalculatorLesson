cc7e77edb793af121c82d116e8d61fffb071663a
Angus McIntyre <angus.mcintyre@exprogroup.com>
2017-09-19 16:00:05 +0100

    Adds addition operation

--- CalculatorLesson/Program.cs
+++ CalculatorLesson/Program.cs
@@ -15,6 +15,22 @@ namespace CalculatorLesson
                 Console.WriteLine("Please type a calculation and press ENTER.");
                 string input = Console.ReadLine();
                 Console.WriteLine($"You typed '{input}'.");
+
+                int result = 0;
+
+                // Parse the user input to get 2 operands and a an operator
+                string[] operands = input.Split('+');
+                if (operands.Length == 2)
+                {
+                    int left;
+                    int right;
+                    if (int.TryParse(operands[0], out left) && int.TryParse(operands[1], out right))
+                    {
+                        result = left + right;
+                    }
+                }
+
+                Console.WriteLine($"The result of your calculation is '{result}'.");
             }
         }
     }
