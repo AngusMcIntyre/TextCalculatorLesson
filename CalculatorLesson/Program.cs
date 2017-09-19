e7d7492d0cd592b8f893add2ed2ba49d4dc43579
Angus McIntyre <angus.mcintyre@exprogroup.com>
2017-09-19 15:15:57 +0100

    Added calculation with test output

--- CalculatorLesson/Program.cs
+++ CalculatorLesson/Program.cs
@@ -10,6 +10,12 @@ namespace CalculatorLesson
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Please type a calculation and press ENTER.");
+                string input = Console.ReadLine();
+                Console.WriteLine($"You typed '{input}'.");
+            }
         }
     }
 }
